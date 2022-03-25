cf11c9b8606c654ae71e80308073ea1dd03c1144
0220482112002 <0220482112002@LAB1MAQ09>
2022-03-25 16:00:14 -0300

    Criação de formulário e pesquisa

--- AulaPesquisar/Controllers/PessoasController.cs
+++ AulaPesquisar/Controllers/PessoasController.cs
@@ -12,9 +12,21 @@ namespace AulaPesquisar.Controllers
             db = _db;   
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string texto)
         {
-            return View();
+            if (string.IsNullOrEmpty(texto))
+            {
+                return View(db.PESSOAS.ToList());
+            }
+            else
+            {
+                return View(db.PESSOAS.Where(a =>
+                    a.nome.Contains(texto) ||
+                    a.cpf.Contains(texto) ||
+                    a.rg.Contains(texto) ||
+                    a.idade.Contains(texto)
+                    ));
+            }
         }
     }
 }
